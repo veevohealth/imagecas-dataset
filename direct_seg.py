89a7ed3508a86909ee68f9ee6e8f9baa5c6c9b0c
Arvind Srivastav <arvind@veevo.health>
2025-12-06 15:53:51 -0800

    simplify configs

--- direct_seg.py
+++ direct_seg.py
@@ -1,25 +1,26 @@
 from scipy.ndimage.interpolation import zoom
-import torch
-import argparse
 import os
-import numpy as np
-from data.Image_loader import CoronaryImage
-from utils.utils import get_csv_split
-from torch.utils.data import DataLoader
-import nibabel as nib
-import torch.optim as optim
-from model.loss import DiceLoss,DiceLoss_v1
-import torch.nn as nn
-import time
-from model.FCN import FCN_Gate, FCN
-import multiprocessing
-from tqdm import tqdm
-import yaml
 import re
-from utils.Calculate_metrics import Cal_metrics
-from utils.utils import reshape_img
-from utils.parallel import parallel
+import time
+
+import nibabel as nib
+import numpy as np
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from hydra.utils import to_absolute_path
+from omegaconf import DictConfig
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import hydra
+
+from data.Image_loader import CoronaryImage
+from model.FCN import FCN_Gate, FCN
+from model.loss import DiceLoss, DiceLoss_v1
+from utils.Calculate_metrics import Cal_metrics
+from utils.parallel import parallel
+from utils.utils import get_csv_split, reshape_img
 
 
 def train(model, criterion, train_loader, opt, device, e):
@@ -107,47 +108,45 @@ def save_picture(pre, affine, img_size, save_name, id):
     os.makedirs(os.path.join(save_name, id), exist_ok=True)
     nib.save(nib.Nifti1Image(pre_label, affine), os.path.join(save_name, id + '/pre_label.nii.gz'))
 
-def args_input():
-    p = argparse.ArgumentParser(description='cmd parameters')
-    p.add_argument('--gpu_index', type=int, default=3)
-    p.add_argument('--config_file', type=str, default='config/config.yaml')
-    p.add_argument('--fold', type=int, default=1)
-    p.add_argument('--load_num', type=int, default=0)
-    p.add_argument('--is_train', type=int, default=1)
-    p.add_argument('--batch_size', type=int, default=2)
-    p.add_argument('--model', type=str, default='FCN_AG')
-    p.add_argument('--channel', type=int, default=4)
-    p.add_argument('--rl', type=int, default=1)
-    p.add_argument('--pools', type=int, default=1)
-    p.add_argument('--num_workers', type=int, default=8)
-    p.add_argument('--is_inference', type=int, default=1)
-    p.add_argument('--loss',type=str,default='Dice')
-    p.add_argument('--epochs',type=int,default=30)
-    return p.parse_args()
+@hydra.main(version_base="1.3", config_path="configs", config_name="train")
+def main(cfg: DictConfig) -> None:
+    """
+    Hydra entrypoint for direct segmentation training / inference.
 
+    Usage examples:
+      - python direct_seg.py
+      - python direct_seg.py train.fold=2 train.model=FCN
+      - python direct_seg.py general.csv_path=data_list/split_v2.csv
+    """
+    general_cfg = cfg.general
+    train_cfg = cfg.train
 
-if __name__ == '__main__':
+    # Paths and global settings (resolve relative paths against original working dir)
+    learning_rate = float(general_cfg.lr)
+    train_path = to_absolute_path(str(general_cfg.data_path))
+    valid_path = to_absolute_path(str(general_cfg.data_path))
+    csv_path = to_absolute_path(str(general_cfg.csv_path))
 
-    ##参数解析
-    args = args_input()
-    gpu_index = args.gpu_index
-    config_file = args.config_file
-    k = args.fold
-    load_num = args.load_num
-    batch_size = args.batch_size
-    model_name = args.model
-    channel = args.channel
-    resolution = args.rl
-    pool_nums = args.pools
-    num_workers = args.num_workers
-    is_train = args.is_train
-    loss_name=args.loss
-    epochs=args.epochs
+    # Training / runtime parameters
+    gpu_index = int(train_cfg.gpu_index)
+    k = int(train_cfg.fold)
+    load_num = int(train_cfg.load_num)
+    batch_size = int(train_cfg.batch_size)
+    model_name = str(train_cfg.model)
+    channel = int(train_cfg.channel)
+    resolution = int(train_cfg.rl)
+    pool_nums = int(train_cfg.pools)
+    num_workers = int(train_cfg.num_workers)
+    is_train = int(train_cfg.is_train)
+    loss_name = str(train_cfg.loss)
+    epochs = int(train_cfg.epochs)
+    is_inference_flag = int(train_cfg.is_inference)
+
     result_path = r'result/Direct_seg'
 
     # Device setup: use CUDA if available, otherwise fall back to CPU
     if torch.cuda.is_available():
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_index)
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_index)
         torch.cuda.set_device(0)
         torch.backends.cudnn.enabled = True
         device = torch.device("cuda")
@@ -168,16 +167,7 @@ if __name__ == '__main__':
     else:
         raise ValueError("没有该级别的分辨率")
 
-    # 从config.yaml里面读取参数
-    with open(config_file) as f:
-        # Use safe_load to avoid deprecated default loader behavior
-        config = yaml.safe_load(f)
-        learning_rate = config['General_parameters']['lr']
-        train_path = config['General_parameters']['data_path']
-        valid_path = config['General_parameters']['data_path']
-        csv_path = config['General_parameters']['csv_path']
-
-    parameter_record = resolution_name + '_%d_' % channel+ loss_name
+    parameter_record = resolution_name + '_%d_' % channel + loss_name
 
     print('model: %s || parameters: %s || %d_fold' % (model_name, parameter_record, k))
 
@@ -194,7 +184,7 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_set, batch_size, num_workers=num_workers, shuffle=True)
     valid_loader = DataLoader(valid_set, batch_size, num_workers=num_workers, shuffle=False)
 
-    # # 网络模型
+    # 网络模型
     if model_name == "FCN":
         net = FCN(channel).to(device)
     elif model_name == "FCN_AG":
@@ -217,9 +207,9 @@ if __name__ == '__main__':
     net = net.to(device)
 
     net_opt = optim.Adam(net.parameters(), lr=learning_rate)
-    if loss_name=='Dice':
+    if loss_name == 'Dice':
         criterion = DiceLoss()
-    elif loss_name=='Dice_dilation':
+    elif loss_name == 'Dice_dilation':
         criterion = DiceLoss_v1()
     else:
         raise ValueError("No loss")
@@ -248,15 +238,17 @@ if __name__ == '__main__':
         record['valid_loss'] = valid_loss_set
         record = pd.DataFrame(record)
         record_name = time.strftime("%Y_%m_%d_%H.csv", time.localtime())
-        record.to_csv(r'%s/%s/%s/fold_%d/%s' % (result_path, model_name, parameter_record, k, record_name), index=False)
+        record.to_csv(r'%s/%s/%s/fold_%d/%s' % (result_path, model_name, parameter_record, k, record_name),
+                      index=False)
 
     # 推断
     train_infer_loader = DataLoader(train_set, 2, num_workers=num_workers, shuffle=False)
     valid_infer_loader = DataLoader(valid_set, 2, num_workers=num_workers, shuffle=False)
 
-    if args.is_inference == 1:
+    if is_inference_flag == 1:
         print('now inference..............')
-        inference(net, criterion, train_infer_loader, valid_infer_loader, device, save_label_path, is_infer_train=True)
+        inference(net, criterion, train_infer_loader, valid_infer_loader, device, save_label_path,
+                  is_infer_train=True)
 
     # 计算最后的dice
     print('now calculate dice...........')
@@ -267,11 +259,15 @@ if __name__ == '__main__':
     p.join()
 
     # 保存结果
-    record_dice={}
+    record_dice = {}
     record_dice['ID'] = ID_list['valid']
-    result=np.array(result)
-    record_dice['dice'] = result[:,0]
-    record_dice['ahd']=result[:,1]
-    record_dice['hd']=result[:,2]
+    result = np.array(result)
+    record_dice['dice'] = result[:, 0]
+    record_dice['ahd'] = result[:, 1]
+    record_dice['hd'] = result[:, 2]
     record_dice = pd.DataFrame(record_dice)
     record_dice.to_csv(r'%s/%s/%s/fold_%d/result.csv' % (result_path, model_name, parameter_record, k), index=False)
+
+
+if __name__ == '__main__':
+    main()
